872187c0880e3fa83f8d1bd25df7827fc2df2c47
Lola <tziganelamaga@gmail.com>
2021-06-11 14:20:41 +0200

    welcome alert

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+window.onload = function () {
+    swal({
+        title: "¡Bienvenida!",
+        text: "En matcheADAs tu objetivo es juntar tres o más items del mismo tipo, ya sea en fila o en columna. Para eso, selecciona un item y a continuación un item adyacente para intercambiarlos de lugar. \n \n Si se forma un grupo, esos items se eliminarán y ganarás puntos. ¡Sigue armando grupos de tres o más antes de que se acabe el tiempo! \n \n Controles \n Click izquierdo: selección. \n Enter o espacio: selección. \n Flechas o WASD: movimiento e intercambio." ,
+        button: "A Jugar",
+    })
+}
